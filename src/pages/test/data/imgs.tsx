3050b6617d3ebebe0fc136c49bf88c49e18b93e7
ALLMMIN <chawadon.mont62@sskru.ac.th>
2024-01-05 10:33:28 +0700

    'test'

--- src/pages/test/data/imgs.tsx
+++ src/pages/test/data/imgs.tsx
@@ -1,10 +1,233 @@
-import a25 from "@assets/ข้อมูลบุคลากร/25.jpg";
+import a1 from "@assets/ข้อมูลบุคลากร/1.jpg";
+import a2 from "@assets/ข้อมูลบุคลากร/2.jpg";
+import a3 from "@assets/ข้อมูลบุคลากร/3.jpg";
+import a4 from "@assets/ข้อมูลบุคลากร/4.jpg";
+import a5 from "@assets/ข้อมูลบุคลากร/5.jpg";
+import a6 from "@assets/ข้อมูลบุคลากร/6.jpg";
+import a7 from "@assets/ข้อมูลบุคลากร/7.jpg";
+import a8 from "@assets/ข้อมูลบุคลากร/8.jpg";
+import a9 from "@assets/ข้อมูลบุคลากร/9.jpg";
+import a10 from "@assets/ข้อมูลบุคลากร/10.jpg";
+import a11 from "@assets/ข้อมูลบุคลากร/11.jpg";
+import a12 from "@assets/ข้อมูลบุคลากร/12.jpg";
+import a13 from "@assets/ข้อมูลบุคลากร/13.jpg";
+import a14 from "@assets/ข้อมูลบุคลากร/14.jpg";
+import a15 from "@assets/ข้อมูลบุคลากร/15.jpg";
+import a16 from "@assets/ข้อมูลบุคลากร/16.jpg";
+import a17 from "@assets/ข้อมูลบุคลากร/17.jpg";
+import a18 from "@assets/ข้อมูลบุคลากร/18.jpg";
+import a19 from "@assets/ข้อมูลบุคลากร/19.jpg";
+import a20 from "@assets/ข้อมูลบุคลากร/20.jpg";
+import a21 from "@assets/ข้อมูลบุคลากร/21.jpg";
 import a22 from "@assets/ข้อมูลบุคลากร/22.jpg";
 import a23 from "@assets/ข้อมูลบุคลากร/23.jpg";
 import a24 from "@assets/ข้อมูลบุคลากร/24.jpg";
+import a25 from "@assets/ข้อมูลบุคลากร/25.jpg";
 import a26 from "@assets/ข้อมูลบุคลากร/26.jpg";
-
+import a27 from "@assets/ข้อมูลบุคลากร/27.jpg";
+import a28 from "@assets/ข้อมูลบุคลากร/28.jpg";
+import a29 from "@assets/ข้อมูลบุคลากร/29.jpg";
+import a30 from "@assets/ข้อมูลบุคลากร/30.jpg";
+import a31 from "@assets/ข้อมูลบุคลากร/31.jpg";
+import a32 from "@assets/ข้อมูลบุคลากร/32.jpg";
+import a33 from "@assets/ข้อมูลบุคลากร/33.jpg";
+import a34 from "@assets/ข้อมูลบุคลากร/34.jpg";
+import a35 from "@assets/ข้อมูลบุคลากร/35.jpg";
+import a36 from "@assets/ข้อมูลบุคลากร/36.jpg";
+import a37 from "@assets/ข้อมูลบุคลากร/37.jpg";
+import a38 from "@assets/ข้อมูลบุคลากร/38.jpg";
+import a39 from "@assets/ข้อมูลบุคลากร/39.jpg";
+import a40 from "@assets/ข้อมูลบุคลากร/40.jpg";
+import a41 from "@assets/ข้อมูลบุคลากร/41.jpg";
+import a42 from "@assets/ข้อมูลบุคลากร/42.jpg";
+import a43 from "@assets/ข้อมูลบุคลากร/43.jpg";
+import a44 from "@assets/ข้อมูลบุคลากร/44.jpg";
+import a45 from "@assets/ข้อมูลบุคลากร/45.jpg";
+import a46 from "@assets/ข้อมูลบุคลากร/46.jpg";
+import a47 from "@assets/ข้อมูลบุคลากร/47.jpg";
+import a48 from "@assets/ข้อมูลบุคลากร/48.jpg";
+import a49 from "@assets/ข้อมูลบุคลากร/49.jpg";
+import a50 from "@assets/ข้อมูลบุคลากร/50.jpg";
+import a51 from "@assets/ข้อมูลบุคลากร/51.jpg";
+import a52 from "@assets/ข้อมูลบุคลากร/52.jpg";
+import a53 from "@assets/ข้อมูลบุคลากร/53.jpg";
+import a54 from "@assets/ข้อมูลบุคลากร/54.jpg";
+import a55 from "@assets/ข้อมูลบุคลากร/55.jpg";
+import a56 from "@assets/ข้อมูลบุคลากร/56.jpg";
+import a57 from "@assets/ข้อมูลบุคลากร/57.jpg";
+import a58 from "@assets/ข้อมูลบุคลากร/58.jpg";
+import a59 from "@assets/ข้อมูลบุคลากร/59.jpg";
+import a60 from "@assets/ข้อมูลบุคลากร/60.jpg";
+import a61 from "@assets/ข้อมูลบุคลากร/61.jpg";
+import a62 from "@assets/ข้อมูลบุคลากร/62.jpg";
+import a63 from "@assets/ข้อมูลบุคลากร/63.jpg";
+import a64 from "@assets/ข้อมูลบุคลากร/64.jpg";
+import a65 from "@assets/ข้อมูลบุคลากร/65.jpg";
+import a66 from "@assets/ข้อมูลบุคลากร/66.jpg";
+import a67 from "@assets/ข้อมูลบุคลากร/67.jpg";
+import a68 from "@assets/ข้อมูลบุคลากร/68.jpg";
+import a69 from "@assets/ข้อมูลบุคลากร/69.jpg";
+import a70 from "@assets/ข้อมูลบุคลากร/70.jpg";
+import a71 from "@assets/ข้อมูลบุคลากร/71.jpg";
+import a72 from "@assets/ข้อมูลบุคลากร/72.jpg";
+import a73 from "@assets/ข้อมูลบุคลากร/73.jpg";
+import a74 from "@assets/ข้อมูลบุคลากร/74.jpg";
+import a75 from "@assets/ข้อมูลบุคลากร/75.jpg";
+import a76 from "@assets/ข้อมูลบุคลากร/76.jpg";
+import a77 from "@assets/ข้อมูลบุคลากร/77.jpg";
+import a78 from "@assets/ข้อมูลบุคลากร/78.jpg";
+import a79 from "@assets/ข้อมูลบุคลากร/79.jpg";
+import a80 from "@assets/ข้อมูลบุคลากร/80.jpg";
+import a81 from "@assets/ข้อมูลบุคลากร/81.jpg";
+import a82 from "@assets/ข้อมูลบุคลากร/82.jpg";
+import a83 from "@assets/ข้อมูลบุคลากร/83.jpg";
+import a84 from "@assets/ข้อมูลบุคลากร/84.jpg";
+import a85 from "@assets/ข้อมูลบุคลากร/85.jpg";
+import a86 from "@assets/ข้อมูลบุคลากร/86.jpg";
+import a87 from "@assets/ข้อมูลบุคลากร/87.jpg";
+import a88 from "@assets/ข้อมูลบุคลากร/88.jpg";
+import a89 from "@assets/ข้อมูลบุคลากร/89.jpg";
+import a90 from "@assets/ข้อมูลบุคลากร/90.jpg";
+import a91 from "@assets/ข้อมูลบุคลากร/91.jpg";
+import a92 from "@assets/ข้อมูลบุคลากร/92.jpg";
+import a93 from "@assets/ข้อมูลบุคลากร/93.jpg";
+import a94 from "@assets/ข้อมูลบุคลากร/94.jpg";
+import a95 from "@assets/ข้อมูลบุคลากร/95.jpg";
+import a96 from "@assets/ข้อมูลบุคลากร/96.jpg";
+import a97 from "@assets/ข้อมูลบุคลากร/97.jpg";
+import a98 from "@assets/ข้อมูลบุคลากร/98.jpg";
+import a99 from "@assets/ข้อมูลบุคลากร/99.jpg";
+import a100 from "@assets/ข้อมูลบุคลากร/100.jpg";
+import a101 from "@assets/ข้อมูลบุคลากร/101.jpg";
+import a102 from "@assets/ข้อมูลบุคลากร/102.jpg";
+import a103 from "@assets/ข้อมูลบุคลากร/103.jpg";
+import a104 from "@assets/ข้อมูลบุคลากร/104.jpg";
+import a105 from "@assets/ข้อมูลบุคลากร/105.jpg";
+import a106 from "@assets/ข้อมูลบุคลากร/106.jpg";
+import a107 from "@assets/ข้อมูลบุคลากร/107.jpg";
+import a108 from "@assets/ข้อมูลบุคลากร/108.jpg";
+import a109 from "@assets/ข้อมูลบุคลากร/109.jpg";
+import a110 from "@assets/ข้อมูลบุคลากร/100.jpg";
+import a111 from "@assets/ข้อมูลบุคลากร/111.jpg";
+import a112 from "@assets/ข้อมูลบุคลากร/112.jpg";
+import a113 from "@assets/ข้อมูลบุคลากร/113.jpg";
+import a114 from "@assets/ข้อมูลบุคลากร/114.jpg";
+import a115 from "@assets/ข้อมูลบุคลากร/115.jpg";
+import a116 from "@assets/ข้อมูลบุคลากร/116.jpg";
+import a117 from "@assets/ข้อมูลบุคลากร/117.jpg";
+import a118 from "@assets/ข้อมูลบุคลากร/118.jpg";
+import a119 from "@assets/ข้อมูลบุคลากร/119.jpg";
+import a120 from "@assets/ข้อมูลบุคลากร/110.jpg";
+import a121 from "@assets/ข้อมูลบุคลากร/121.jpg";
+import a122 from "@assets/ข้อมูลบุคลากร/122.jpg";
+import a123 from "@assets/ข้อมูลบุคลากร/123.jpg";
+100;
 const imgs = [
+  {
+    title: "นางสาวภัคสุรีย์รัช ศัลให้อยู่สุข",
+    department: "เจ้าหน้าที่ธุรการ งานส่งเสริมการค้า ฯ",
+    thumbnailUrl: a1,
+  },
+  {
+    title: "นายสุทัศน์ ทองจุล",
+    department: "เจ้าหน้าที่ นักการ-ภารโรง",
+    thumbnailUrl: a2,
+  },
+  {
+    title: "นายมานะศักดิ์ อารมณ์สวะ",
+    department: "ครูแผนก: ช่างกลโรงงาน",
+    thumbnailUrl: a3,
+  },
+  {
+    title: "นายประภาส นามโคตร",
+    department: "ครูแผนกวิชา: ช่างกลโรงงาน",
+    thumbnailUrl: a4,
+  },
+  {
+    title: "นายไกรศร ผิวผัน",
+    department: "ครูแผนกวิชา: ช่างยนต์",
+    thumbnailUrl: a5,
+  },
+  {
+    title: "นางเพ็ชรา โหม่งมาตร์",
+    department: "ครูแผนกวิชา: การโรงแรม",
+    thumbnailUrl: a6,
+  },
+  {
+    title: "นายบุญญฤทธื์ แสงทอง",
+    department: "ครูแผนก: ช่างกลโรงงาน",
+    thumbnailUrl: a7,
+  },
+  {
+    title: "นางสาววิมาดา คำแสนราด",
+    department: "นศ.ฝ.ปสก.วชช.ค แผนกช่างเชื่อมโลหะ",
+    thumbnailUrl: a8,
+  },
+  {
+    title: "นางสาวสุทธิดา เกษี",
+    department: "ครูแผนกวิชา: การตลาด",
+    thumbnailUrl: a9,
+  },
+  {
+    title: "นายธนัญชัย สิงห์อุดม",
+    department: "ครูแผนกวิชา: การตลาด",
+    thumbnailUrl: a10,
+  },
+  {
+    title: "นายขวัญ ชิณวงษ์",
+    department: "ครูแผนกวิชา: ช่างยนต์",
+    thumbnailUrl: a11,
+  },
+  {
+    title: "นางวิไลพร อรรคชัย",
+    department: "เจ้าหน้าที่ธุรการ งานวิจัย/งานประกันคุณภาพ ฯ",
+    thumbnailUrl: a12,
+  },
+  {
+    title: "นางสาวกชพรรณ ปะเพราะตา",
+    department: "เจ้าหน้าที่ธุรการ งานพัสดุ",
+    thumbnailUrl: a13,
+  },
+  {
+    title: "นายณฐกร สุขพิสิษฐ์",
+    department: "เจ้าหน้าที่ธุรการ งานกิจกรรม",
+    thumbnailUrl: a14,
+  },
+  {
+    title: "Miss.Wang Ruyan",
+    department: "ครูอาสาสมัครสอนภาษาจีน",
+    thumbnailUrl: a15,
+  },
+  {
+    title: "นายเรืองยศ อรัญรักษ์",
+    department: "ครูแผนกวิชา: สามัญสัมพันธ์",
+    thumbnailUrl: a16,
+  },
+  {
+    title: "นางสาวสุพัตรา กาวัน",
+    department: "ครูแผนกวิชา: สามัญสัมพันธ์",
+    thumbnailUrl: a17,
+  },
+  {
+    title: "นางสาววิมลรัตน์ โททอง",
+    department: "เจ้าหน้าที่ธุรการ งานวางแผน ฯ",
+    thumbnailUrl: a18,
+  },
+  {
+    title: "นายสุภาพ กันเทพา",
+    department: "เจ้าหน้าที่บริการทั่วไป",
+    thumbnailUrl: a19,
+  },
+  {
+    title: "นายวุฒิชัย โพธิ์ขาว",
+    department: "พนักงานขับรถ",
+    thumbnailUrl: a20,
+  },
+  {
+    title: "จ่าสอบเอกชาติชาย ฝอยทอง",
+    department: "ครูแผนกวิชา: ช่างยนต์",
+    thumbnailUrl: a21,
+  },
   {
     title: "นางสาวขนิษฐา แสงเนตร",
     department: "ครูแผนกวิชา: สามัญสัมพันธ์",
@@ -29,6 +252,496 @@ const imgs = [
     title: "นายณฐกร สุขพิสิษฐ",
     department: "เจ้าหน้าที่ธุรการ งานกิจกรรม ",
     thumbnailUrl: a26,
+  },
+  {
+    title: "นายณฐกร สุขพิสิษฐ",
+    department: "เจ้าหน้าที่ธุรการ งานกิจกรรม ",
+    thumbnailUrl: a26,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a27,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a28,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a29,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a30,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a31,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a32,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a33,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a34,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a35,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a36,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a37,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a38,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a39,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a40,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a41,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a42,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a43,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a44,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a45,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a46,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a47,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a48,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a49,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a50,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a51,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a52,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a53,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a54,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a55,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a56,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a57,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a58,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a59,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a60,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a61,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a62,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a63,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a64,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a65,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a66,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a67,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a68,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a69,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a70,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a71,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a72,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a73,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a74,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a75,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a76,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a77,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a78,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a79,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a80,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a81,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a82,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a83,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a84,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a85,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a86,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a87,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a88,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a89,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a90,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a91,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a92,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a93,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a94,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a95,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a96,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a97,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a98,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a99,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a100,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a101,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a102,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a103,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a104,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a105,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a106,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a107,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a108,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a109,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a110,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a111,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a112,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a113,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a114,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a115,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a116,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a117,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a118,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a119,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a120,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a121,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a122,
+  },
+  {
+    title: ".........",
+    department: ".........",
+    thumbnailUrl: a123,
   },
 ];
 
