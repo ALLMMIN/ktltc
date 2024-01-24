16718cb793345b8dc49f49ed42497795776e5d08
ALLMMIN <chawadon.mont62@sskru.ac.th>
2024-01-24 10:12:14 +0700

    'v3'

--- src/components/database/DataPressRelease.tsx
+++ src/components/database/DataPressRelease.tsx
@@ -131,7 +131,7 @@ import img62 from "@assets/error.webp";
 import img63 from "@assets/ข่าวประชาสัมพันธ์/41/2.webp";
 import img64 from "@assets/ข่าวประชาสัมพันธ์/42/01.webp";
 import img65 from "@assets/ข่าวประชาสัมพันธ์/43/22.webp";
-import img66 from "@assets/ข่าวประชาสัมพันธ์/44/1.1.webp";
+// import img66 from "@assets/ข่าวประชาสัมพันธ์/44/1.1.webp";
 import img67 from "@assets/ข่าวประชาสัมพันธ์/44/6.webp";
 import img68 from "@assets/ข่าวประชาสัมพันธ์/45/1.webp";
 import img69 from "@assets/ข่าวประชาสัมพันธ์/46/56.webp";
@@ -209,7 +209,7 @@ export const DataPressRelease = [
     name: `พระบาทสมเด็จพระปรเมนทรรามาธิบดีศรีสินทรมหาวชิราลงกรณ์
     พระวชิรเกล้าเจ้าอยู่หัว พระราชทานพระบรมราโชวาท
     สำหรับลงพิมพ์ในหนังสือวันเด็กแห่งชาติปี ๒๕๖๗`,
-    backgroundImage: img66,
+    // backgroundImage: img66,
     review: ` `,
     url: <KingVajiraklao />,
   },
